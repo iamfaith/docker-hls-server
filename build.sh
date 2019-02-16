b5a1aa74edbba7d62eef72a12b87bf47996a581f
xianzixiang <xianzixiang@gmail.com>
2019-02-17 00:55:54 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd `dirname $0`
-sudo docker build -t hls-server --build-arg STREAM_SERVER=$STREAM_SERVER --build-arg STREAM_KEY=$STREAM_KEY .
+sudo docker build -t xianzixiang/hls-server --build-arg STREAM_SERVER=$STREAM_SERVER --build-arg STREAM_KEY=$STREAM_KEY .
